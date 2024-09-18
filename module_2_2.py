d540a5996fc370e3f9d084e8db9358c7a3666410
ttcmd-main <118116436+ttcmd-main@users.noreply.github.com>
2024-09-18 18:37:18 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -3,7 +3,7 @@ second = int(input())
 third = int(input())
 if first == second == third:
     print(3)
-elif first == second or third or second == first or third:
+elif first == second or first == third or second == third::
     print(2)
 else:
     print(0)
